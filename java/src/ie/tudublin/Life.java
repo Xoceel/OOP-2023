68f50f76c0790c5f204f1cf00a4fc249fc94f43e
Lee <echomartin12@gmail.com>
2023-03-09 00:32:57 +0000

    glider and GosperGun

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -8,6 +8,7 @@ public class Life extends PApplet
 {
 	LifeBoard board;
 	boolean looping = true;
+	int mouseEffects = 1;
 
 	public void settings()
 	{
@@ -18,7 +19,7 @@ public class Life extends PApplet
 		colorMode(RGB);
 		background(0);
 		frameRate(5);
-		board = new LifeBoard(20, this);
+		board = new LifeBoard(100, this);
 		board.randomise();
 		frameRate(50);
 	}
@@ -44,17 +45,27 @@ public class Life extends PApplet
 		}
 
 		if (key == ' ') {
-			if (looping) {
-				noLoop();
-				looping = false;
-			} else {
-				loop();
-				looping  = true;
-			}
+			looping = board.pausePlay(looping);
+		}
+
+		if (key == 'e') {
+			mouseEffects++;
 		}
 	}
 
 	public void mouseMoved() {
-		board.mouseDraw(mouseX, mouseY);
+		if (mouseEffects % 3 == 0) {
+			board.dragDraw(mouseX, mouseY);
+		}
+	}
+
+	public void mouseClicked() {
+		if (mouseEffects % 3 == 1) {
+			board.gliderDraw(mouseX, mouseY);
+		}
+
+		if (mouseEffects % 3 == 2) {
+			board.gosperGunDraw(mouseX, mouseY);
+		}
 	}
 }
--- java/src/ie/tudublin/LifeBoard.java
+++ java/src/ie/tudublin/LifeBoard.java
@@ -161,9 +161,46 @@ public class LifeBoard {
                 }
             }
         }
+
     }
 
-    public void mouseDraw(int y, int x) {
+    public void dragDraw(int y, int x) {
         board[(int)(x/cellWidth)][(int)(y/cellWidth)] = true;
+    }
+
+    public void gliderDraw(int y, int x) {
+        int rowPos = (int)(x / cellWidth);
+        int colPos = (int)(y / cellWidth);
+
+        for(int i = -1 ; i <= 1 ; i ++)
+        {
+            for (int j = -1 ; j <= 1 ; j ++)
+            {
+                if ((i == -1 && j == 0) || (i == 0 && j == 1) || i == 1) {
+                    board[i + rowPos][j + colPos] = true;
+                }
+            }
+        }
+        
+    }
+
+    public void gosperGunDraw(int x, int y) {
+        int rowPos = (int)(x / cellWidth);
+        int colPos = (int)(y / cellWidth);
+        int[] xCoords = {0, 0, 1, 1, 10, 10, 10, 11, 11, 12, 12, 13, 13, 13, 13, 14, 15, 15, 16, 16, 16, 17, 20, 20, 20, 21, 21, 21, 22, 22, 24, 24, 24, 24, 34, 34, 35, 35};
+        int[] yCoords = {4, 5, 4, 5, 4, 5, 6, 3, 7, 2, 8, 2, 8, 1, 9, 6, 3, 7, 4, 5, 6, 5, 2, 3, 4, 2, 3, 4, 1, 5, 0, 1, 5, 6, 2, 3, 2, 3};
+
+        if(((size - rowPos) > 36) && ((size - colPos) > 9)) {
+            for(int i = 0; i<=36; i++) {
+                board[((int)((yCoords[i] * cellWidth) + y) / (int)cellWidth)][(int)(((xCoords[i] * cellWidth) + x) / (int) cellWidth)] = true;
+            }
+        }
+    } 
+
+    public boolean pausePlay(boolean looping) {
+        if (looping) {
+            p.noLoop();
+            return false;
+        } else p.loop(); return true;
     }
 }
